aa5247e8b97cfe7821d023076c6c5522f673f832
Beomi <latheledusjp@gmail.com>
2018-09-26 15:24:49 +0900

    Add Sentry for error tracking

--- whereismycustoms/settings.py
+++ whereismycustoms/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import dj_database_url
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +139,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+# Sentry Error tracking
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY"),
+    integrations=[DjangoIntegration()]
+)
